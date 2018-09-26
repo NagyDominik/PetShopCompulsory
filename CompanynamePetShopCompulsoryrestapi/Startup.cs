48aec9287e3ff47959c683e3ef51cd4e5a2e2bed
Dominik Nagy <32298986+NagyDominik@users.noreply.github.com>
2018-09-26 12:36:15 +0200

    defaultConnection

--- CompanynamePetShopCompulsoryrestapi/Startup.cs
+++ CompanynamePetShopCompulsoryrestapi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using PetShop.Infrastructure.Data;
 using PetShopCompulsory.Core;
@@ -17,30 +18,55 @@ namespace CompanynamePetShopCompulsoryrestapi
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IConfiguration _conf { get; }
+
+        private IHostingEnvironment _env { get; set; }
+
+        public Startup(IHostingEnvironment env)
         {
-            Configuration = configuration;
+            _env = env;
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            _conf = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        //public Startup(IConfiguration configuration)
+        //{
+        //    Configuration = configuration;
+        //}
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IPetRepository, PetRepository>();
-            services.AddScoped<IOwnerRepository, OwnerRepository>();
-            services.AddScoped<IPetService, PetService>();
-            services.AddScoped<IOwnerService, OwnerService>();
-            //services.AddDbContext<PetShopCompulsoryContext>(opt => opt.UseInMemoryDatabase("DBOne"));
+            //services.AddDbContext<CustomerAppContext>(
+            //    opt => opt.UseInMemoryDatabase("ThaDB")
+            //    );
 
-            services.AddDbContext<PetShopCompulsoryContext>(opt => opt.UseSqlite("Data Source=petshop.db"));
+            if (_env.IsDevelopment()) {
+                services.AddDbContext<PetShopCompulsoryContext>(
+                    opt => opt.UseSqlite("Data Source=customerApp.db"));
+            }
+            else if (_env.IsProduction()) {
+                services.AddDbContext<PetShopCompulsoryContext>(
+                    opt => opt
+                        .UseSqlServer(_conf.GetConnectionString("defaultConnection")));
+            }
+
+            services.AddScoped<IPetRepository, PetRepository>();
+            services.AddScoped<IPetService, PetService>();
+
+            services.AddScoped<IOwnerRepository, OwnerRepository>();
+            services.AddScoped<IOwnerService, OwnerService>();
 
             services.AddMvc().AddJsonOptions(options => {
-                options.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
-                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
             });
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,18 +74,22 @@ namespace CompanynamePetShopCompulsoryrestapi
         {
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
-
-                using (IServiceScope scope = app.ApplicationServices.CreateScope()) {
-                    PetShopCompulsoryContext ctx = scope.ServiceProvider.GetService<PetShopCompulsoryContext>();
+                using (var scope = app.ApplicationServices.CreateScope()) {
+                    var ctx = scope.ServiceProvider.GetService<PetShopCompulsoryContext>();
                     DBInitializer.SeedDB(ctx);
                 }
             }
             else {
+                using (var scope = app.ApplicationServices.CreateScope()) {
+                    var ctx = scope.ServiceProvider.GetService<PetShopCompulsoryContext>();
+                    ctx.Database.EnsureCreated();
+                }
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
+
 }
